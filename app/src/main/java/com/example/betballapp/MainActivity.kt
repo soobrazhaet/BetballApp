4c0f472779106a58bccf84bdf157b2e90840506e
soobrazhaet <ichevichelov03@mail.ru>
2023-06-16 20:35:18 +0300

    second commit

--- app/src/main/java/com/example/betballapp/MainActivity.kt
+++ app/src/main/java/com/example/betballapp/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.betballapp
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.betballapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appNavHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        appNavHostFragment = supportFragmentManager.findFragmentById(R.id.appNavHostFragment) as NavHostFragment
+        binding.bottomNavigationView.setupWithNavController(appNavHostFragment.findNavController())
     }
 }
---
+++ app/src/main/java/com/example/betballapp/ui/fragments/GoalsFragment.kt
@@ -0,0 +1,14 @@
+package com.example.betballapp.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.betballapp.R
+
+
+class GoalsFragment : Fragment(R.layout.fragment_goals) {
+
+
+}
