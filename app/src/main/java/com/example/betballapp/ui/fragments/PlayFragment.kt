fabe0d5c7595532c2d4da31300bd99ec94a48db0
soobrazhaet <ichevichelov03@mail.ru>
2023-06-17 17:07:28 +0300

    third commit

--- app/src/main/java/com/example/betballapp/ui/fragments/PlayFragment.kt
+++ app/src/main/java/com/example/betballapp/ui/fragments/PlayFragment.kt
@@ -1,13 +1,28 @@
 package com.example.betballapp.ui.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.betballapp.R
+import com.example.betballapp.databinding.FragmentPlayBinding
 
 
 class PlayFragment : Fragment(R.layout.fragment_play) {
 
+    lateinit var binding: FragmentPlayBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentPlayBinding.inflate(inflater, container, false)
+
+        binding.playBtnStart.setBackgroundColor(Color.RED)
+
+        return binding.root
+    }
 }
